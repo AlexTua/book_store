5e92aac8e92652e7abf47b761aa0161607f557b3
AlexTua <taler97@gmail.com>
2017-02-22 23:08:17 +0200

    update config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "bookstore_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { :host => 'bookstore17.herokuapp' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
