95bd1721368af1217a9d80ef067d4c6b8fbf9150
AlexTua <taler97@gmail.com>
2017-02-24 14:32:13 +0200

    add book, author, category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170222221843) do
+ActiveRecord::Schema.define(version: 20170224104800) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "authors_books", id: false, force: :cascade do |t|
+    t.integer "book_id",   null: false
+    t.integer "author_id", null: false
+    t.index ["book_id", "author_id"], name: "index_authors_books_on_book_id_and_author_id", unique: true
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.decimal  "price",       precision: 10, scale: 2
+    t.integer  "quantity"
+    t.integer  "category_id"
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
+    t.index ["category_id"], name: "index_books_on_category_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'ffaker'
+
+Category.create(title: "Mobile development")
+Category.create(title: "Photo")
+Category.create(title: "Web design")
+Category.create(title: "Web development")
+
+Category.find_each do |category|
+  5.times do |n|
+    author = Author.create(first_name: FFaker::Name.first_name, last_name: FFaker::Name.last_name)
+    book = Book.create(category: category, title: FFaker::Book.title, price: rand(1..99), 
+      quantity: rand(50..200))
+    book.authors << author
+  end
+end
