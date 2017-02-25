a8863fc08557cb60926c3d9fea048f7e5180347e
AlexTua <taler97@gmail.com>
2017-02-25 13:04:20 +0200

    fix migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170224104800) do
+ActiveRecord::Schema.define(version: 20170225110127) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "first_name"
@@ -30,9 +30,9 @@ ActiveRecord::Schema.define(version: 20170224104800) do
     t.text     "description"
     t.decimal  "price",       precision: 10, scale: 2
     t.integer  "quantity"
-    t.integer  "category_id"
     t.datetime "created_at",                           null: false
     t.datetime "updated_at",                           null: false
+    t.integer  "category_id"
     t.index ["category_id"], name: "index_books_on_category_id"
   end
 
