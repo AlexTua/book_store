5100a13686da6500340de77e36d229481bcd1191
AlexTua <taler97@gmail.com>
2017-02-25 12:51:40 +0200

    add catalog

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root    'home_page#index'
+  resources :categories, only: [:show], path: 'catalog'
   get     '/doc',          to: 'home_page#documentation'
 end
