f0c0172ebed9b54d4f614696e656219cffe238db
AlexTua <taler97@gmail.com>
2017-02-28 01:28:34 +0200

    Add forgot password page

--- Gemfile
+++ Gemfile
@@ -20,6 +20,7 @@ gem 'devise'
 gem 'omniauth'
 gem 'omniauth-facebook'
 gem 'ffaker'
+gem 'kaminari'
 
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
-    @sorted_books = SortBooksService.new(params, @category).sort_books
+    @sorted_books = SortBooksService.new(params, @category).sort_books.page(params[:page])
   end
 end
