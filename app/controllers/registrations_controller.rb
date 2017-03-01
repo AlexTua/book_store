066d875e95189fe85b518299ef7fc13a16a4955a
AlexTua <taler97@gmail.com>
2017-03-01 20:34:04 +0200

    add book page

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,3 +1,16 @@
 class RegistrationsController < Devise::RegistrationsController
+  protected
+
+  def update_resource(resource, params)
+    if params.include?(:current_password)
+      resource.update_with_password(params)
+    else
+      resource.update_without_password(params)
+    end
+  end
   
+  def after_update_path_for(resource)
+    Rails.logger.debug("My object: #{params.inspect}")
+    edit_user_registration_path()
+  end
 end
