1c32ceb80fb80d8f768a5b48595333d0d4340ac3
AlexTua <taler97@gmail.com>
2017-03-06 16:59:28 +0200

    change book quantity

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,25 +1,28 @@
 class OrderItemsController < ApplicationController
   def create
     @order = current_order
-    @order.save
     @order_item = @order.order_items.new(order_item_params)
-    if @order.save
+    if @order.valid?
+      @order.save 
       session[:order_id] = @order.id
-    else
-      redirect_to session[:forwarding_url], alert: "Item was not added."
+      cahnge_book_quantity(-@order_item.quantity)
     end
   end
 
   def update
-    @order = current_order
-    @order_item = @order.order_items.find(params[:id])
-    @order_item.update_attributes(order_item_params)
-    redirect_to cart_path, notice: "Item updated."
+    @order_item = current_order.order_items.find(params[:id])
+    if @order_item.quantity > params[:order_item][:quantity].to_i  
+      update_order_items(1)
+    elsif @order_item.book.in_stock?
+      update_order_items(-1)
+    else
+      redirect_to cart_path, alert: "Item is out of stock."
+    end
   end
 
   def destroy
-    @order = current_order
-    @order_item = @order.order_items.find(params[:id])
+    @order_item = current_order.order_items.find(params[:id])
+    cahnge_book_quantity(@order_item.quantity)
     @order_item.destroy
     redirect_to cart_path, notice: "Item deleted."
   end
@@ -28,5 +31,16 @@ class OrderItemsController < ApplicationController
 
   def order_item_params
     params.require(:order_item).permit(:quantity, :book_id)
+  end
+
+  def cahnge_book_quantity(int)
+    @order_item.book.quantity += int
+    @order_item.book.save
+  end
+
+  def update_order_items(int)
+    cahnge_book_quantity(int)
+    @order_item.update(order_item_params) 
+    redirect_to cart_path, notice: "Item updated."
   end
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -6,8 +6,17 @@ class OrderItem < ApplicationRecord
 
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :order_id, :book_id, presence: true
-
+  validate :book_quantity, on: :create
+  
   def total_price
     book.price * quantity
+  end
+
+  private
+
+  def book_quantity
+    if book.quantity < quantity
+      errors.add(:order_item, "is out of stock")
+    end
   end
 end
