2afa312f1ace53d86c04833133710403a757f222
AlexTua <taler97@gmail.com>
2017-03-13 01:26:28 +0200

    save order after log in

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,12 +4,15 @@ class Users::OmniauthCallbacksController < ApplicationController
 
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication
+      save_user_to_order
       flash[:notice] = "Facebook signed in successfully."
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
   end
+
+  private
 
   def failure
     flash[:alert] = "Facebook log in failed"
