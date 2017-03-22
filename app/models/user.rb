78992cef76f232fbeda80114d05d2a82c0fb32dd
AlexTua <taler97@gmail.com>
2017-03-23 00:26:57 +0200

    fix FB address

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ApplicationRecord
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
     end
+    return user if user.get_address("billing")
     user.addresses.create(first_name: auth.info.first_name, 
                           last_name: auth.info.last_name, address_type: "billing")
     user
